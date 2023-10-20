699843dfc7b73482201a15114581ee0bca306775
Samanvith <Samanvith2005@gmail.com>
2023-10-20 12:51:39 +0530

    Added react-router-dom

---
+++ netflix/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {BrowserRouter,Route,Routes} from "react-router-dom"
+import Netflix from './pages/Netflix'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+     <Route  exact path ="/" element={<Netflix/>}/>
+     <Route  exact path ="/login" element={<Login/>}/>
+     <Route  exact path ="/signup" element={<Signup/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
